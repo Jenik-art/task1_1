a977fc31b7a55156cb19cef641d15c7fbb308bc9
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-11 13:53:46 +0300

    Задание 1_1 - выделены вспомогательные методы и классы

--- task_1_1.py
+++ task_1_1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -30,19 +31,19 @@ class task_1_1(unittest.TestCase):
         # open group page
         wd.find_element_by_link_text("groups").click()
 
-    def create_new_group(self, wd, name, header, footer):
+    def create_new_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
 
 
     def test_task_1_1(self):
@@ -51,7 +52,7 @@ class task_1_1(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_new_group(wd, name="task_1_1", header="Header", footer="Footer")
+        self.create_new_group(wd, Group(name="task_1_1", header="Header", footer="Footer"))
         #submit group creation
         wd.find_element_by_name("submit").click()
         #return to group page
