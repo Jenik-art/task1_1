81be383a96227ca056e26bdd4184fa80cf1220b1
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-11 13:12:24 +0300

    Задание 1_1 - разбивка кода на понятные компоненты + добавление одного пропущеннго степа Рефакторинг кода, но есть проблема: созданные методы создаются после их объявления.

--- task_1_1.py
+++ task_1_1.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -14,13 +13,11 @@ class task_1_1(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
-    
-    def test_task_1_1(self):
-        success = True
-        wd = self.wd
-        #open home page
+    def open_home_page(self, wd):
+
         wd.get("http://localhost/addressbook")
-        #login
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -28,11 +25,15 @@ class task_1_1(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        #open group page
+
+    def open_group_page(self, wd):
+        # open group page
         wd.find_element_by_link_text("groups").click()
-        #init group creation
+
+    def create_new_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
-        #fill group form
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("task")
@@ -45,6 +46,15 @@ class task_1_1(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("Footer")
+
+
+    def test_task_1_1(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_new_group(wd)
         #submit group creation
         wd.find_element_by_name("submit").click()
         #return to group page
@@ -52,7 +62,11 @@ class task_1_1(unittest.TestCase):
         #logout
         wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
-    
+
+
+
+
+
     def tearDown(self):
         self.wd.quit()
 
