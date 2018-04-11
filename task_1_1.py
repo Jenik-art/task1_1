ae12b78a099bb15090e92e50d8f8edefcc604e94
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-11 13:26:37 +0300

    Задание 1_1 - чистим код, делаем его более удобным для многократного исползование по сердствам расхардкоживания передываемых параметров: узернэйм,пароль,название группы, название хедера и футера.

--- task_1_1.py
+++ task_1_1.py
@@ -17,44 +17,41 @@ class task_1_1(unittest.TestCase):
 
         wd.get("http://localhost/addressbook")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_group_page(self, wd):
         # open group page
         wd.find_element_by_link_text("groups").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("task")
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("task_1_1")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Header")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Footer")
+        wd.find_element_by_name("group_footer").send_keys(footer)
 
 
     def test_task_1_1(self):
         success = True
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_new_group(wd)
+        self.create_new_group(wd, name="task_1_1", header="Header", footer="Footer")
         #submit group creation
         wd.find_element_by_name("submit").click()
         #return to group page
