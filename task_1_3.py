b1319af15406f1ee6e498e7f88064097c7da3731
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-13 15:18:11 +0300

    Задание 1_3 - оптимизация кода, перенес метод Оупен хоум пейдж в метод Логин

--- task_1_3.py
+++ task_1_3.py
@@ -1,82 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
 from contact import Contact
+from application import Application
+import pytest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
-
-class task_1_3(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def init_add_contact(self, wd, contact):
-        wd.find_element_by_link_text("add new").click()
-        # fill form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.homephone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.workphone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_task_1_3(self):
-        success = True
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.init_add_contact(wd, Contact( "Ivan", "Ivanovich", "Ivanov", "IvanCo", "New York, Brighton beach 100-200","+1111111111", "+12222222", "+3333333", "ivanovivan@ivanco.com"))
-        self.logout(wd)
-        self.assertTrue(success)
-
-
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_task_1_3(app):
+    app.login(username="admin", password="secret")
+    app.init_add_contact(Contact( "Ivan", "Ivanovich", "Ivanov", "IvanCo", "New York, Brighton beach 100-200","+1111111111", "+12222222", "+3333333", "ivanovivan@ivanco.com"))
+    app.logout()
