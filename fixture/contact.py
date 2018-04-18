270dccaa3890460931f447d395666126f853686a
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-18 21:52:55 +0300

    Задание 1_3 - выделен отдельный класс для создания нового контакта Задание 1_1 - тест переведен на pytest + создан новый класс для создания группы + тест подключен ко всем вспомогательным классам
    Добавляю забытые в прошлом коммите новые классы

---
+++ fixture/contact.py
@@ -0,0 +1,38 @@
+
+class contactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def add(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        # fill form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.workphone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
