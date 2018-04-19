fddad87be6620a6885479d24d60f6c0692ed4516
evgeny.artsiomenko <jenik.art@gmail.com>
2018-04-19 21:32:01 +0300

    Задание 6. Создал тест по изменению группы, + создал тест по удаленю контакта

--- fixture/group.py
+++ fixture/group.py
@@ -32,5 +32,27 @@ class groupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page(wd)
 
+    def edit_first_group(self,group):
+        wd = self.app.wd
+        # open group page
+        wd.find_element_by_link_text("groups").click()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page(wd)
+
+
+
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
