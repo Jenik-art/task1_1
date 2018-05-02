1054f51f3d8008fb730a72671b162b80a295d411
evgeny.artsiomenko <jenik.art@gmail.com>
2018-05-02 14:38:00 +0300

    Задание 11 - ошибка, наверное, в методе open_groups_page

--- model/group.py
+++ model/group.py
@@ -1,5 +1,6 @@
 class Group:
-    def __init__(self,header = None,footer = None ,name= None):
+    def __init__(self,header = None,footer = None ,name= None, id = None):
         self.name=name
         self.header=header
         self.footer=footer
+        self.id = id
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,10 +4,12 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="New group", header="Header", footer="Footer"))
-    app.session.logout()
+   old_groups = app.group.get_group_list()
+   app.group.create(Group(name="New group", header="Header", footer="Footer"))
+   new_groups = app.group.get_group_list()
+   assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+   app.group.create(Group(name="", header="", footer=""))
 
 
